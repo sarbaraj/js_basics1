9924003abcc77720264434b1d2cc6f5bf6945753
sarbaraj <rajclasher321@gmail.com>
2026-07-16 05:45:00 +0545

    variables

---
+++ 1.variable.js
@@ -0,0 +1,49 @@
+//! dynamic typed , interpreted , single threaded language
+
+// console.log("Hello world");
+console.log("a");
+// console.log("b");
+// console.log("c");
+
+//! variable
+// console.log(10);
+
+//* variable declaration methods
+//? var let const
+
+// int a =20
+
+var a = 10; // {a : 10}
+
+console.log(a); //
+
+a = 400;
+
+a = "a";
+
+a = true;
+
+console.log(a); //
+
+let b = 20;
+
+console.log(b);
+
+b = 200;
+
+// let b = "b"; //
+
+console.log(b);
+
+const c = 45;
+
+// const c = "c";
+
+// c = 78;
+
+console.log(c);
+console.log("end");
+
+// let d;
+// var e;
+// const f;
